9b1752dc9b6260bec1725ba0a49406e2eb9045fd
kojot1995 <wojak.karol95@gmail.com>
2022-10-03 21:46:23 +0200

    Add comments and call function

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-console.log('test text');
-
+// create arrays
 const horoscopeSigns = ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces'];
 const luck = ['good luck', 'bad luck'];
 const action = ['trust no one', 'spend more time outside', 'avoid people', 'make some friends'];
 
+// select random index
 const horoscopeSignsIndex = Math.floor(Math.random() * horoscopeSigns.length);
 const luckIndex = Math.floor(Math.random() * luck.length);
 const actionIndex = Math.floor(Math.random() * action.length);
 
+// print formatted message
 const printMessage = () => {
     console.log(`Your sign is ${horoscopeSigns[horoscopeSignsIndex]}.`);
     console.log(`You are having ${luck[luckIndex]}.`);
     console.log(`You should ${action[actionIndex]}.`);
 }
                     
+printMessage();
