61fb67d85f77fafc095d890a8c979587e903974c
kojot1995 <wojak.karol95@gmail.com>
2022-10-03 21:24:07 +0200

    Add three arrays

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 console.log('test text');
+
+const horoscopeSigns = ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces'];
+const luck = ['good luck', 'bad luck'];
+const action = ['trust no one', 'spend more time outside', 'avoid people', 'make some friends'];
+
+                    
